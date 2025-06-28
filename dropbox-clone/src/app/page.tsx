30966fda178a1e8e0abc9557690ff80d8a69419b
yungiy <devyungiy@gmail.com>
2025-06-28 17:33:09 +0900

    Initial commit
    Project scaffolding and initial file setup.

---
+++ dropbox-clone/src/app/page.tsx
@@ -0,0 +1,10 @@
+import UI from "./ui";
+
+export const metadata = {
+  title: "Minibox",
+  description: "A minimalistic Dropbox clone",
+};
+
+export default function Home() {
+  return <UI />;
+}
